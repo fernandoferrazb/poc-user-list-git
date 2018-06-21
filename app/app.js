aa3da0a576bf33fbd71d42863f12a4a0c54d3cca
fernandoferrazb <fernandoferrazbarreto@gmail.com>
2018-06-21 15:50:32 -0300

    Config node and modules

--- app/app.js
+++ app/app.js
@@ -1,16 +1,4 @@
-var express = require('express');
-var cons = require('consolidate');
-var app = express();
+//Config server
+var app = require('../config/server');
 
-// assign the dust engine to .dust filess
-app.engine('dust', cons.dust);
-app.set('view engine', 'dust');
-app.set('views', __dirname + '/../views');
-
-app.get('/',function(req,res){
-	res.render('index');
-});
-
-app.listen(8000, function(){
-	console.log('localhost:8000')
-});
+app();
---
+++ config/server.js
@@ -0,0 +1,19 @@
+module.exports = function(){
+	var express = require('express');
+
+	//assign the dust engine to .dust files
+	var app = express();
+	var cons = require('consolidate');
+	app.engine('dust', cons.dust);
+	app.set('view engine', 'dust');
+	app.set('views', __dirname + '/../views');
+
+	//module routes
+	var routes = require('../app/routes/web');
+	routes(app);
+
+	//server local
+	app.listen(8000, function () {
+		console.log('localhost:8000')
+	});
+}
