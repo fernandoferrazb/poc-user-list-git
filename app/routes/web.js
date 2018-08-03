4b27a383095067aed01b7a00e3c7edd5df246a75
Fernando Ferraz <fernandoferrazbarreto@gmail.com>
2018-08-03 18:29:10 -0300

    Search with name user

--- app/routes/web.js
+++ app/routes/web.js
@@ -2,7 +2,33 @@ module.exports = function(app) {
 	var express = require('express');
 	//map statics
 	app.use(express.static(__dirname + './../../client'));
+
+	//route index
 	app.get('/', function (req, res) {
 		res.render('index');
+	});
+
+	//route consult
+	app.get('/resultsearch', function(req,res) {
+		var request = require('request');
+
+		var options = {
+			url: 'https://api.github.com/?access_token=abeabc08fd655a774ae225f28ea604d8bc19ab36',
+			url: 'https://api.github.com/users/fernandoferrazb',
+			headers: {
+				'User-Agent': 'request'
+			}
+		};
+
+		function callback(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var gitConsult = JSON.parse(body);
+				res.render('resultSearch', {
+					gitConsult
+				});
+				console.log(gitConsult);
+			}
+		}
+		request(options, callback);
 	});
 }
--- client/javascript/query-git.js
+++ client/javascript/query-git.js
@@ -1,3 +1,17 @@
 (function () {
-    console.log("Javascript");
+	var search = document.querySelector('.container__search-block__button');
+	var inputSearch = document.querySelector('.container__search-block__input');
+		
+	search.addEventListener("click", function(){
+		var ajax = new XMLHttpRequest();
+		ajax.open('GET', '/resultSearch', true);
+		ajax.send();
+		ajax.onreadystatechange = function () {
+			if (ajax.readyState == 4 && ajax.status == 200) {
+				var data = ajax.responseText;
+				console.log(data);
+				document.getElementsByClassName('conteudo-render')[0].innerHTML = data;
+		}
+	}});
+
 })();
