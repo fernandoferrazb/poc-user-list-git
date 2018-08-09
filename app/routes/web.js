9bfe5167a5876dd598c553a310a392a962cb26e3
Fernando Ferraz <fernandoferrazbarreto@gmail.com>
2018-08-09 17:38:38 -0300

    Update routes and search

--- app/routes/web.js
+++ app/routes/web.js
@@ -2,33 +2,31 @@ module.exports = function(app) {
 	var express = require('express');
 	//map statics
 	app.use(express.static(__dirname + './../../client'));
+	
+	//authentication api github
+	app.get('/auth', function(req,res) {
+		var request = require('request');
+		var authgithub = 'https://api.github.com/?access_token=abeabc08fd655a774ae225f28ea604d8bc19ab36';
+		request(authgithub, {
+			headers: {
+				'User-Agent': 'request'
+			}
+		}).pipe(res);
+	});
 
 	//route index
 	app.get('/', function (req, res) {
 		res.render('index');
 	});
 
-	//route consult
-	app.get('/resultsearch', function(req,res) {
+	//route consult with proxy
+	app.get('/result', function(req,res) {
 		var request = require('request');
-
-		var options = {
-			url: 'https://api.github.com/?access_token=abeabc08fd655a774ae225f28ea604d8bc19ab36',
-			url: 'https://api.github.com/users/fernandoferrazb',
+		var newurl = `https://api.github.com/users/${req.query.search}`;
+		request(newurl, {
 			headers: {
 				'User-Agent': 'request'
 			}
-		};
-
-		function callback(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var gitConsult = JSON.parse(body);
-				res.render('resultSearch', {
-					gitConsult
-				});
-				console.log(gitConsult);
-			}
-		}
-		request(options, callback);
+		}).pipe(res);
 	});
 }
--- client/javascript/query-git.js
+++ client/javascript/query-git.js
@@ -1,17 +1,16 @@
 (function () {
 	var search = document.querySelector('.container__search-block__button');
-	var inputSearch = document.querySelector('.container__search-block__input');
 		
 	search.addEventListener("click", function(){
+		var nomeUser = document.querySelector('.container__search-block__input').value;
 		var ajax = new XMLHttpRequest();
-		ajax.open('GET', '/resultSearch', true);
+		ajax.open('GET', '/result?search=' + nomeUser, true);
 		ajax.send();
 		ajax.onreadystatechange = function () {
 			if (ajax.readyState == 4 && ajax.status == 200) {
-				var data = ajax.responseText;
-				console.log(data);
-				document.getElementsByClassName('conteudo-render')[0].innerHTML = data;
+				var gitConsult = JSON.parse(ajax.responseText);
+				document.getElementsByClassName('conteudo-render')[0].innerHTML = gitConsult.login;
+			}
 		}
-	}});
-
+	});
 })();
