3d689311d97df9484dc1803414b16a2aa8051326
Fernando Ferraz <fernandoferrazbarreto@gmail.com>
2018-08-13 09:51:31 -0300

    Update style and image

--- client/javascript/query-git.js
+++ client/javascript/query-git.js
@@ -3,7 +3,9 @@
 		
 	search.addEventListener("click", function(){
 		var nomeUser = document.querySelector('.container__search-block__input').value;
-		var createtext = document.getElementsByClassName('conteudo-render')[0];
+		var createaLi = document.createElement("li");
+		var createaLiTwo = document.createElement("li");
+		var createaLithree = document.createElement("img");
 
 		var ajax = new XMLHttpRequest();
 		ajax.open('GET', '/result?search=' + nomeUser, true);
@@ -11,9 +13,22 @@
 		ajax.onreadystatechange = function () {
 			if (ajax.readyState == 4 && ajax.status == 200) {
 				var gitConsult = JSON.parse(ajax.responseText);
-				createtext.innerHTML = gitConsult.login + gitConsult.id + gitConsult.avatar_url;
+				createaLi.innerHTML = "Nome: " + gitConsult.name;
+				createaLi.className = "conteudo-render__nome";
+				createaLiTwo.innerHTML = "Login: " + gitConsult.login;
+				createaLiTwo.className = "conteudo-render__user";
+				createaLithree.src = gitConsult.avatar_url;
+				createaLithree.className = "conteudo-render__avatar";
+				createaLithree.innerHTML = "Avatar: " + createaLithree.src; 
+				document.getElementsByClassName('conteudo-render')[0].appendChild(createaLi);
+				document.getElementsByClassName('conteudo-render')[0].appendChild(createaLiTwo);
+				document.getElementsByClassName('conteudo-render')[0].appendChild(createaLithree);
 			}
-			createtext.innerHTML = 'Usuário inexistente';
+			else {
+				var renderText = 'Usuário não encontrado';
+				createaLi.innerHTML = renderText;
+				document.getElementsByClassName('conteudo-render')[0].appendChild(createaLi);
+			}
 		}
 	});
 })();
